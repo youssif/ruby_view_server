afc9e1cb274cc8bff73a133268b8a028bca7d9c9
MakerSquare Student <student@lab-4.att.net>
2013-06-25 11:14:07 -0500

    Initial commit

--- erb_example.rb
+++ erb_example.rb
@@ -1,5 +1,11 @@
 require 'erb'
 
-x = 42
+x = 46
+name = 'Youssif'
+school = 'MakerSquare'
 template = ERB.new "The value of x is: <%= x %>"
+template1 = ERB.new "My name is: <%= name %>"
+template2 = ERB.new "I go to: <%= school %>"
 puts template.result(binding)
+puts template1.result(binding)
+puts template2.result(binding)
